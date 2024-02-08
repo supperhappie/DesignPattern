68a7829f3c66bd195e14e1912ac7dd50104adfa8
supper happie <kwtdevel@gmail.com>
2024-02-08 09:51:27 +0900

    edit sample

--- prototype/Main.java
+++ prototype/Main.java
@@ -5,11 +5,15 @@ public class Main {
         ConcretePrototype prototype = new ConcretePrototype(0);
         System.out.println("Object ID1 : " + prototype.getId());
 
-        ConcretePrototype prototype2 = (ConcretePrototype)prototype.clone();
+        ConcretePrototype prototype2 = prototype;
         System.out.println("Object ID2 : " + prototype2.getId());
+
+        ConcretePrototype clonedPrototype = (ConcretePrototype)prototype.clone();
+        System.out.println("Cloned Object : " + clonedPrototype.getId());
 
         prototype2.setId(2);
         System.out.println("Object ID1 : " + prototype.getId());
         System.out.println("Object ID2 : " + prototype2.getId());
+        System.out.println("Cloned Object ID : " + clonedPrototype.getId());
     }
 }
